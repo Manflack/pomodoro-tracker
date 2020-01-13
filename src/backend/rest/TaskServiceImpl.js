00b4fd4209ac56aa2393ee6872d50595377cb5a0
Manflack <marianofauez@live.com>
2020-01-12 23:43:10 -0300

    added morgan for loggin and nodemon for reload

--- src/backend/rest/TaskServiceImpl.js
+++ src/backend/rest/TaskServiceImpl.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
 app.listen(5000);
+app.use(morgan("dev"));
 
 app.get("/listTaskTodo", (req, res) => {
   res.end("this is a execution");
